756a469307991cde51e345cb0ad4812da570f873
Charly3012 <carlocoronadosilva975@gmail.com>
2026-05-21 01:55:08 -0600

    ci: pipi para integracion y pruebas unitarias

--- core/php/Login.php
+++ core/php/Login.php
@@ -6,53 +6,22 @@
  * Date: 07/02/2016
  * Time: 06:29 PM
  */
-include "DataBaseManager.php";
-require_once("Session.php");
-<<<<<<< HEAD
-=======
-require_once("interfaces/DatabaseInterface.php");
-require_once("interfaces/SessionInterface.php");
-require_once("adapters/DatabaseAdapter.php");
-require_once("adapters/SessionAdapter.php");
-require_once("services/AuthService.php");
+require_once __DIR__ . '/DataBaseManager.php';
+require_once __DIR__ . '/Session.php';
+require_once __DIR__ . '/interfaces/DatabaseInterface.php';
+require_once __DIR__ . '/interfaces/SessionInterface.php';
+require_once __DIR__ . '/adapters/DatabaseAdapter.php';
+require_once __DIR__ . '/adapters/SessionAdapter.php';
+require_once __DIR__ . '/services/AuthService.php';
 
->>>>>>> origin/main
 
 $username = $_POST["username"];
 $password = $_POST["password"];
 
 
-<<<<<<< HEAD
-$database = DataBaseManager::getInstance();
-$query = "Select * FROM  usuario WHERE nombre = '$username' AND clave = '$password'";
-$result = $database->realizeQuery($query);
-verifyLogin($result, $username);
-
-function verifyLogin($result, $username) {
-    $message = null;
-    $session = new session();
-
-    if (count($result) > 0) {
-
-        $user = array();
-
-        $user['type'] = $result[0]['tipo'];
-
-        //$session->set("user", $username);
-        $session->set("user", $result[0]['id']);
-        $usersList[] = $user;
-
-        echo json_encode($usersList);
-    } else {
-        echo json_encode($message);
-    }
-}
-
-=======
 $db = new DatabaseAdapter();
 $session = new SessionAdapter();
 $authService = new AuthService($db, $session);
 
 $response = $authService->login($username, $password);
 echo json_encode($response);
->>>>>>> origin/main
